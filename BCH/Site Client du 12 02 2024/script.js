6592ebdfe9ecb5be51771a3f5efbf2ce0847052c
vianneysite <105672199+vianneysite@users.noreply.github.com>
2024-02-29 23:24:17 +0100

    Add files via upload

--- BCH/Site Client du 12 02 2024/script.js
+++ BCH/Site Client du 12 02 2024/script.js
@@ -2,10 +2,16 @@ var isMenuVisible = false;
 function displayMenu() {
     var menu = document.getElementById('menu');
     if (isMenuVisible == false) {
-        menu.style.display = 'flex';
+        console.log('ouvrir');
+        document.getElementById("open").classList.remove("on");
+        document.getElementById("close").classList.add("on");
+        document.getElementById("navbar_smartphone").classList.add("on");
         isMenuVisible = true;
     } else {
-        menu.style.display = 'none';
+        console.log('fermer');
+        document.getElementById("open").classList.add("on");
+        document.getElementById("close").classList.remove("on");
+        document.getElementById("navbar_smartphone").classList.remove("on");
         isMenuVisible = false;
 
     }
@@ -25,16 +31,4 @@ window.addEventListener('scroll', () => {
     }
 });
 
-function OpenMenu() {
-    console.log('ouvrir');
-    document.getElementById("open").classList.remove("on");
-    document.getElementById("close").classList.add("on");
-    document.getElementById("navbar_smartphone").classList.add("on");
-}
 
-function CloseMenu() {
-    console.log('fermer');
-    document.getElementById("open").classList.add("on");
-    document.getElementById("close").classList.remove("on");
-    document.getElementById("navbar_smartphone").classList.remove("on");
-}
